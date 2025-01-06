12ca8976137a5784325bd57225c344ce52d3c0f1
argentina9631 <argentina9631@gmail.com>
2025-01-06 03:50:47 -0300

    Login nuevo ordenado no anda

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import './App.css';
-import LoginForm from './components/LoginForm'; // Componente del formulario de login
+import React from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <LoginForm />
     </div>
   );
