23bb5fd45e298b1a141636667c568bc1cd07f32f
argentina9631 <argentina9631@gmail.com>
2025-01-09 04:55:52 -0300

    Cerrar sesion

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Verificar si el token está en el localStorage
+    const token = localStorage.getItem('token');
+    if (!token) {
+      // Si no hay token, redirigir al login
+      navigate('/');
+    }
+  }, [navigate]);
+
+  const handleLogout = () => {
+    // Eliminar el token de localStorage
+    localStorage.removeItem('token');
+    
+    // Redirigir a la ruta personalizada, en este caso, la ruta de inicio
+    window.location.href = 'http://localhost:3001';  // Redirigir al inicio de sesión externo
+  };
+
   return (
     <div>
-      <h2>Bienvenido al Dashboard</h2>
+      <h1>Bienvenido a la pantalla principal</h1>
+      <button onClick={handleLogout}>Cerrar sesión</button>
     </div>
   );
 };
