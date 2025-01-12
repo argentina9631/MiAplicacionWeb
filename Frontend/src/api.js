7c07e37abb251ad84f7c03a03a3d493aa5b5aaef
argentina9631 <argentina9631@gmail.com>
2025-01-12 18:55:56 -0300

    probando backend web

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,8 +1,15 @@
 // frontend/src/api.js
 import axios from 'axios';
 
+// Detectar si estamos en producción o desarrollo
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://bzfd5tinydz29pndrsn6-mysql.services.clever-cloud.com/api' // URL del backend en Clever Cloud
+    : 'http://localhost:3000/api'; // Desarrollo local
+
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+  baseURL, // URL base configurada dinámicamente
 });
 
 export default api;
+
