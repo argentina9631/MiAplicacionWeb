3ba935e199125dfbb5e6bd015e3cd3df0329914b
miaplicacionweb <argentina9631@gmail.com>
2025-01-21 17:16:18 -0300

    prueba001

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,14 +1,12 @@
 // frontend/src/api.js
 import axios from 'axios';
 
-// Detectar si estamos en producción o desarrollo
-const baseURL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080/api' // URL del backend en desarrollo (puedes ajustar el puerto si es necesario)
-    : 'https://app-e1cc2c91-dfc6-49c5-8a1c-6a1907e248e3.cleverapps.io/api'; // URL del backend en Clever Cloud (producción)
+// Usa la variable de entorno REACT_APP_API_URL
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api'; // Default a localhost si no está configurado
 
 const api = axios.create({
   baseURL, // URL base configurada dinámicamente
 });
 
 export default api;
+
