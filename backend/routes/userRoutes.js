ac9cc7479c4489ec9e9a96e03d0f06787699c16e
miaplicacionweb <argentina9631@gmail.com>
2025-02-09 13:44:17 -0300

    L10

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,4 @@
-// backend/routes/userRoutes.js
+//ruta: backend/routes/userRoutes.js
 const express = require("express");
 const router = express.Router();
 const { loginUser } = require("../controllers/userController");
