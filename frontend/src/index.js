94545e36bd1078d7c3142898887a6cc61b1c22bc
miaplicacionweb <argentina9631@gmail.com>
2025-02-27 19:34:59 -0300

    Ñ35

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from 'react-router-dom';  // Importa BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* Envuelve la app en BrowserRouter */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
