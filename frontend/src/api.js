c8ab01e29d1ca53eea58a32229788f8287dbc6d3
miaplicacionweb <argentina9631@gmail.com>
2025-02-27 15:52:06 -0300

    Ñ5

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // frontend/src/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
 
 const api = axios.create({
   baseURL: API_URL,
