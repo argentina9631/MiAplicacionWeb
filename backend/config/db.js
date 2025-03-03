199ee4507eee153aa1dcb68e096d74e54b49c266
miaplicacionweb <argentina9631@gmail.com>
2025-03-03 19:38:51 -0300

    log func capaz 1

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+//backend/config/db.js
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'b8biz3pozkccjo4cgtlo-mysql.services.clever-cloud.com', //localmente es 'localhost'
+  user: 'umjqezvfshle1hsn',//localmente es 'root'
+  password: 'Jk4asxYTAhD8hd367zgo',//localmente es '9631argentina'
+  database: 'b8biz3pozkccjo4cgtlo',//localmente es 'db_miaplicacionweb'
+  port:'3306',//localmente no va esta linea
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error de conexión: " + err.stack);
+    return;
+  }
+  console.log("Conectado a la base de datos con el ID " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ backend/hashPassword.js
@@ -0,0 +1,13 @@
+//backend/hashPassword.js
+const bcrypt = require('bcryptjs');
+
+// Cambia esta contraseña por la que quieras hashear
+const password = 'miPasswordSeguro';
+
+bcrypt.hash(password, 10, (err, hash) => {
+  if (err) {
+    console.error('Error al generar el hash:', err);
+  } else {
+    console.log('Contraseña hasheada:', hash);
+  }
+});
