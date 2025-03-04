9753b037e10e93b09f4169e056efd528d7c51339
miaplicacionweb <argentina9631@gmail.com>
2025-03-04 01:24:55 -0300

    login capaz q funciona 50

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,11 +2,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',  // Usar variable de entorno o localhost
-  user: process.env.DB_USER || 'root',  // Usar variable de entorno o 'root' localmente
-  password: process.env.DB_PASSWORD || '9631argentina',  // Usar variable de entorno o contraseña local
-  database: process.env.DB_NAME || 'db_miaplicacionweb',  // Usar variable de entorno o base de datos local
-  port: process.env.DB_PORT || 3306,  // Usar variable de entorno o puerto local
+  host: process.env.DB_HOST, //|| 'localhost',   Usar variable de entorno o localhost
+  user: process.env.DB_USER, //|| 'root',  // Usar variable de entorno o 'root' localmente
+  password: process.env.DB_PASSWORD, //|| '9631argentina',   Usar variable de entorno o contraseña local
+  database: process.env.DB_NAME, //|| 'db_miaplicacionweb',   Usar variable de entorno o base de datos local
+  port: process.env.DB_PORT, //|| 3306,   Usar variable de entorno o puerto local
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0
