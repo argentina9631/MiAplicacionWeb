5510adb82ab9b0ff6397ecf7e3db4b79cf0c0967
miaplicacionweb <argentina9631@gmail.com>
2025-03-04 22:12:14 -0300

    clever cloud prueba

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,13 @@
 //backend/config/db.js
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST, //|| 'localhost',   Usar variable de entorno o localhost
-  user: process.env.DB_USER, //|| 'root',  // Usar variable de entorno o 'root' localmente
-  password: process.env.DB_PASSWORD, //|| '9631argentina',   Usar variable de entorno o contraseña local
-  database: process.env.DB_NAME, //|| 'db_miaplicacionweb',   Usar variable de entorno o base de datos local
-  port: process.env.DB_PORT, //|| 3306,   Usar variable de entorno o puerto local
+  host: process.env.MYSQL_ADDON_HOST, //|| 'localhost',   Usar variable de entorno o localhost
+  user: process.env.MYSQL_ADDON_USER, //|| 'root',   Usar variable de entorno o 'root' localmente
+  password: process.env.MYSQL_ADDON_PASSWORD, //|| '9631argentina',   Usar variable de entorno o contraseña local
+  database: process.env.MYSQL_ADDON_DB, //|| 'db_miaplicacionweb',   Usar variable de entorno o base de datos local
+  port: process.env.MYSQL_ADDON_PORT, //|| 3306,   Usar variable de entorno o puerto local
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0
